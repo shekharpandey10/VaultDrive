25b39cdfc2826a977e2bf9b6a5674761e2ebcd35
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-06-16 23:42:05 +0530

    router added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,24 +1,30 @@
-const express=require("express")
+const express = require("express")
 const { body, validationResult } = require('express-validator');
-const router=express.Router()
+const router = express.Router()
 
-router.get('/',(req,res)=>[
+router.get('/', (req, res) => [
     res.render('register')
 ])
 
-router.get('/register',(req,res)=>{
+router.get('/register', (req, res) => {
     res.render('register')
 })
 
 router.post('/register',
-    body('email').trim().isEmail(),
-    body('password').trim().isLength({min:5}),
-    body('name').trim().isLength({min:3}),
-    (req,res)=>{
-    const {name,email,password}=req.body
-    console.log(req.body)
-    res.send("user registered" +name)
-})
+    body('email').trim().isEmail().isLength({ min: 13 }),
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim().isLength({ min: 3 }),
+    (req, res) => {
+
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors:errors.array(),
+                message:'Invalid data'
+            })
+        }
+        res.send("user registered")
+    })
 
 
-module.exports=router
+module.exports = router
